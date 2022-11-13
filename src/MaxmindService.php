6157f658bd9b51be4fc5cfd9ce14bd423ed74455
alexander <alex.shklyarenko22@gmail.com>
2022-11-13 12:04:19 +0200

    seco dasda commit

---
+++ src/MaxmindService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AingShMax\MaxMindServicePackage;
+
+use Alexander\GeoServiceInterfaceTest\GeoServiceInterface;
+use GeoIp2\Database\Reader;
+use GeoIp2\Exception\AddressNotFoundException;
+use GeoIp2\Exception\GeoIp2Exception;
+
+
+class MaxmindService implements GeoServiceInterface
+{
+    protected $_reader;
+    protected $_data;
+    /**
+     * @Package AingSh\Alexander\PackageHillelGeoTest v2.0.0
+     */
+    public function __construct()
+    {
+        $this->_reader = new Reader(
+            base_path() . DIRECTORY_SEPARATOR .
+            'database' . DIRECTORY_SEPARATOR .
+            'geoip' . DIRECTORY_SEPARATOR .
+            'GeoLite2-Country.mmdb'
+        );
+    }
+
+
+    public function parse(string $ip): void
+    {
+        try {
+            $this->_data = $this->_reader->country($ip);
+        } catch (AddressNotFoundException $exception) {
+
+        }
+
+    }
+
+    public function getIsoCode(): ?string
+    {
+        return $this->_data->continent->code ?? null;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->_data->country->isoCode ?? null;
+    }
+}
